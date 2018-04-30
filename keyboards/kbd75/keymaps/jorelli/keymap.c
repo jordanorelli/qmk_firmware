8fd107f1351a33484d4947680120d1c2eb51a10c
Jordan Orelli <jordanorelli@gmail.com>
2018-04-29 21:55:34 -0500

    audio control keys

--- keyboards/kbd75/keymaps/jorelli/keymap.c
+++ keyboards/kbd75/keymaps/jorelli/keymap.c
@@ -6,7 +6,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // Layer 0: Qwerty Layer
   KEYMAP(
-      LT(2, KC_ESC), KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, MO(1), KC_DEL,
+      KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, LT(2, KC_F12), KC_PSCR, MO(1), KC_DEL,
       KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRV, KC_HOME,
       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC, KC_PGUP,
       KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_PGDN,
@@ -22,6 +22,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS, KC_TRNS, KC_TRNS, BL_DEC, BL_TOGG, BL_INC, BL_STEP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
+  // Layer 2: Media Controls
+  KEYMAP(
+   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU,
+   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+
   // Layer Template
   // KEYMAP(
   //  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -33,64 +42,62 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
-  // switch (id) {
-  // }
-  return MACRO_NONE;
-}
+// const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
+//   return MACRO_NONE;
+// }
 
-void matrix_init_user(void) {
-}
+// void matrix_init_user(void) {
+// }
 
-void matrix_scan_user(void) {
-}
+// void matrix_scan_user(void) {
+// }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  return true;
-}
+// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//   return true;
+// }
 
-void led_set_user(uint8_t usb_led) {
-  if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
-    DDRB |= (1 << 2);
-    PORTB &= ~(1 << 2);
-  } else {
-    DDRB &= ~(1 << 2); PORTB &= ~(1 << 2);
-  }
-}
+// void led_set_user(uint8_t usb_led) {
+//   if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
+//     DDRB |= (1 << 2);
+//     PORTB &= ~(1 << 2);
+//   } else {
+//     DDRB &= ~(1 << 2); PORTB &= ~(1 << 2);
+//   }
+// }
 
-enum function_id {
-  SHIFT_ESC,
-};
+// enum function_id {
+//   SHIFT_ESC,
+// };
 
-const uint16_t PROGMEM fn_actions[] = {
-  [0]  = ACTION_FUNCTION(SHIFT_ESC),
-};
+// const uint16_t PROGMEM fn_actions[] = {
+//   [0]  = ACTION_FUNCTION(SHIFT_ESC),
+// };
 
-void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
-  static uint8_t shift_esc_shift_mask;
-  switch (id) {
-    case SHIFT_ESC:
-      shift_esc_shift_mask = get_mods()&MODS_CTRL_MASK;
-      if (record->event.pressed) {
-        if (shift_esc_shift_mask) {
-          add_key(KC_GRV);
-          send_keyboard_report();
-        } else {
-          add_key(KC_ESC);
-          send_keyboard_report();
-        }
-      } else {
-        if (shift_esc_shift_mask) {
-          del_key(KC_GRV);
-          send_keyboard_report();
-        } else {
-          del_key(KC_ESC);
-          send_keyboard_report();
-        }
-      }
-      break;
-  }
-}
+// void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
+//   static uint8_t shift_esc_shift_mask;
+//   switch (id) {
+//     case SHIFT_ESC:
+//       shift_esc_shift_mask = get_mods()&MODS_CTRL_MASK;
+//       if (record->event.pressed) {
+//         if (shift_esc_shift_mask) {
+//           add_key(KC_GRV);
+//           send_keyboard_report();
+//         } else {
+//           add_key(KC_ESC);
+//           send_keyboard_report();
+//         }
+//       } else {
+//         if (shift_esc_shift_mask) {
+//           del_key(KC_GRV);
+//           send_keyboard_report();
+//         } else {
+//           del_key(KC_ESC);
+//           send_keyboard_report();
+//         }
+//       }
+//       break;
+//   }
+// }
 
 // uint32_t layer_state_set_user(uint32_t state) {
 // }
